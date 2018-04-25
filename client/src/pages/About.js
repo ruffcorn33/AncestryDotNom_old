43c97e1c47db89cbab78a4bfd2d63284382c3c75
Craig Christensen <ruffcorn@gmail.com>
2018-04-25 16:21:57 -0500

    more formatting

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,7 +6,7 @@ import Col from "../components/Col";
 
 const About = () => (
   <div>
-    <Hero backgroundImage="http://www.culinaryone.com/wp-content/uploads/2012/12/baking_and_pastry_careers.png">
+    <Hero backgroundImage="http://squawalpine.com/sites/default/files/styles/large/public/multiple_medias/media_family-reunion-HC.jpg?itok=Q5NLI7rv">
       <h1>Ancestry*dot*Nom</h1>
       <h2>Where families meet to share recipes</h2>
     </Hero>
@@ -19,7 +19,7 @@ const About = () => (
       <Row>
         <Col size="md-12">
           <p>
-AncestyDotNom is a place where families can share and preserve recipes.  Remember Grandma's Thanksgiving relish?  Let's preserve that recipe here.  Your cousin remembers there being more orange zest?  She can add a note to the recipe.  Your Uncle Bob has been bragging that abouts the best damn grilled chicken ever?  Let's share.  Who remembers Aunt Betty's Jello salad with shaved carrots, cottage cheese and sliced green olives?  Should we make that recipe available for future generations?  Maaaybe....  You decide because this is your family cookbook and you can do whatever you want with it.
+AncestyDotNom is a place where families can share and preserve recipes.  Remember Grandma's Thanksgiving relish?  Let's preserve that recipe here.  Your cousin remembers there being more orange zest?  She can do that because 'a little too much' has always been her defining trait, right?  Your Uncle Bob has been bragging about the best damn grilled chicken ever?  Let's share.  Who remembers Aunt Betty's Jello salad with shaved carrots, cottage cheese and sliced green olives?  Should we make that recipe available for future generations?  Maaaybe....  You decide because this is your family cookbook!
           </p>
         </Col>
       </Row>
